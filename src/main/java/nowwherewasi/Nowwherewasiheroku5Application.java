a1bb8d36c8d0e53b265cf63f93e5aa71e13f4220
aseem191 <aseem191@gmail.com>
2018-08-23 12:58:43 -0700

    wetr

---
+++ src/main/java/nowwherewasi/Nowwherewasiheroku5Application.java
@@ -0,0 +1,12 @@
+package nowwherewasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nowwherewasiheroku5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nowwherewasiheroku5Application.class, args);
+	}
+}
