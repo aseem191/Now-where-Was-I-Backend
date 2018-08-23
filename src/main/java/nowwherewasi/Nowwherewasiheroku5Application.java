2f3500d930d2b1385d32a7e026b59f48a303d104
aseem191 <aseem191@gmail.com>
2018-08-23 13:10:00 -0700

    REt

--- src/main/java/nowwherewasi/Nowwherewasiheroku5Application.java
+++ src/main/java/nowwherewasi/Nowwherewasiheroku5Application.java
@@ -1,10 +1,35 @@
 package nowwherewasi;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class Nowwherewasiheroku5Application {
+	
+	@Autowired
+	 private DataSource dataSource;
+
+	@RequestMapping("/dbadd")
+	  String db(Map<String, Object> model) {
+	    try (Connection connection = dataSource.getConnection()) {
+	      Statement stmt = connection.createStatement();
+	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Thought (thought TEXT, place TEXT, time INTEGER)");
+	      return "db";
+	    } catch (Exception e) {
+	      model.put("message", e.getMessage());
+	      return "error";
+	    }
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Nowwherewasiheroku5Application.class, args);
