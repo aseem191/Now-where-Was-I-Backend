7f149bb561b0667896405f30b4590a1c0a404057
aseem191 <aseem191@gmail.com>
2018-08-24 22:49:13 -0700

    Added CrossOrigin

--- src/main/java/nowwherewasi/Nowwherewasiheroku5Application.java
+++ src/main/java/nowwherewasi/Nowwherewasiheroku5Application.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@CrossOrigin
 @Controller
 @SpringBootApplication
 public class Nowwherewasiheroku5Application {
